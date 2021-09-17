793af0fe0cac67eaa206c7c37a695b9b45f0b1ce
rajumsrk1112 <39462575+rajumsrk1112@users.noreply.github.com>
2021-09-17 19:05:51 +0530

    added button

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Script is working OMG!")
+/*console.log("Script is working OMG!");
+
+let firstName=prompt("Enter your first name");
+
+let lastName=prompt("Enter your last name");
+
+function greetUser(fName,lName)
+{
+    alert("Hello "+fName+" "+lName+"!");
+}
+
+greetUser(firstName,lastName);
+*/
+
+let submitButton=document.querySelector("#submit");
+
+submitButton.addEventListener("click",()=>{alert("button clicked!")});
