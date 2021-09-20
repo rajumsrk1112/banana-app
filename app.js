37b35d59ec3ca39e5bc06e1cfcdb490f6f71519c
rajumsrk1112 <39462575+rajumsrk1112@users.noreply.github.com>
2021-09-20 15:16:33 +0530

    all changes done

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 let messageInput = document.getElementById("message");
-
 let getMessageBtn = document.querySelector("#get-message");
-
 let outputMessage = document.querySelector(".output-text");
 
-function generateUrl(text) {
-  return `https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${text}`;
-}
-
-let url = "";
-
-function fetchCall(link) {
-  fetch(link)
-    .then(response => response.json())
-    .then(json => console.log(json.contents.translated));
-}
-
 getMessageBtn.addEventListener("click", function () {
-  console.log("Input message: " + messageInput.value);
-  outputMessage.innerHTML = messageInput.value;
-  url = generateUrl(messageInput.value);
-  fetchCall(url);
+  console.log(messageInput.value);
+  fetch(
+    `https://api.funtranslations.com/translate/minion.json?text=${messageInput.value}`
+  )
+    .then(response => response.json())
+    .then(json => {
+      outputMessage.innerText = json.contents.translated;
+    });
 });
